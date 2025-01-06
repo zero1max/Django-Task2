0fb9058cc2992ab906c1d79e47bec0b31a0bfd07
Nigmatjonov Muhammadjon <148071379+zero1max@users.noreply.github.com>
2025-01-06 11:31:59 +0500

    Update Commit

--- core/root/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Contact, Category
-
-# Create your views here.
-def home(request):
-    users = Contact.objects.all()
-    categories = Category.objects.all()
-    return render(request, 'home.html', {'users': users, 'categories': categories})
